1029fda0c4f285c4e2fd1625474df0eb07224abb
shadab akhtar <shadabakhtar200@gmail.com>
2020-08-10 14:17:41 -0400

    Problem 1

---
+++ revasses/src/main/java/com/ex/revasess/Problem1.java
@@ -0,0 +1,10 @@
+package com.ex.revasess;
+
+public class Problem1 {
+
+    public boolean compareString(String str1, String str2){
+        if(str1.equals(str2))
+            return true;
+        else return false;
+    }
+}
